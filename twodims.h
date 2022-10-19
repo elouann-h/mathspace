a2891ef1042bb493b544d115ebdd87d025671fe6
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-10-19 18:58:59 +0200

    Documented

--- twodims.h
+++ twodims.h
@@ -5,9 +5,6 @@
 #ifndef MATHSPACE_TWODIMS_H
 #define MATHSPACE_TWODIMS_H
 
-/*
- * Point in 2 Dimensional Space
- */
 struct Point2D {
     double x;
     double y;
@@ -16,10 +13,6 @@ extern const struct Point2DClass {
     struct Point2D (*new)(double x, double y);
 } Point2D;
 
-/*
- * Segment in 2 Dimensional Space
- * Use two structures of Point2D as the end points
- */
 struct Segment2D {
     struct Point2D firstExtremity, secondExtremity;
     double (*lengthSegment2D)(struct Segment2D *this);
@@ -28,10 +21,6 @@ extern const struct Segment2DClass {
     struct Segment2D (*new)(struct Point2D firstExtremity, struct Point2D secondExtremity);
 } Segment2D;
 
-/*
- * Square in a 2 Dimensional Space
- * Use four structures of Point2D as the corners
- */
 struct Square2D {
     struct Point2D topLeftVertex;
     double width;
@@ -42,10 +31,6 @@ extern const struct Square2DClass {
     struct Square2D (*new)(struct Point2D topLeftVertex, double width);
 } Square2D;
 
-/*
- * Triangle in a 2 Dimensional Space
- * Use three structure of Point2D as the corners
- */
 struct Triangle2D {
     struct Point2D firstVertex, secondVertex, thirdVertex;
     double (*areaTriangle2D)(struct Triangle2D *this);
