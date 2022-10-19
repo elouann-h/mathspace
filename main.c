7ea41bb1b4671f564503a60260e0a8c464f90e9e
PxndxDev <80158828+PxndxDev@users.noreply.github.com>
2022-10-19 22:09:53 +0200

    Fixed bug

--- main.c
+++ main.c
@@ -7,15 +7,5 @@
 #include <stdio.h>
 
 int main(void) {
-    // You will need three points (instances of Point2D)
-    struct Point2D firstVertex = Point2D.new(1, 1);
-    struct Point2D secondVertex = Point2D.new(2, 4);
-    struct Point2D thirdVertex = Point2D.new(1, 6);
-    // Create a triangle
-    struct Triangle2D triangle = Triangle2D.new(firstVertex, secondVertex, thirdVertex);
-    // Attributes
-    double triangleArea = triangle.areaTriangle2D(&triangle);
-    printf("The area of the triangle is: %f", triangleArea);
-
     return 0;
 }
